4e881ebe02965ac5732cc43ebeaf37057e76a881
dubalsheetal <dubalsheetal0190@gmail.com>
2024-07-31 16:00:51 +0530

    Javascript typeOf assignment
    Javascript typeOf assignment

---
+++ 02-typeOf.js
@@ -0,0 +1,35 @@
+console.log("=============1==========");
+var bankName;
+console.log("Bank Name Is:",bankName);
+bankName="SBI Bank";
+console.log("Bank Name Is:",bankName);
+var bankNameTypeOf=typeof bankName;
+console.log("Bank Name Datatype Is:",bankNameTypeOf);
+console.log("=============2==========");
+var marks="90%"; 
+console.log("Marks are:",marks);
+var markstypeOf=typeof marks;
+console.log("Datatype of marks:",markstypeOf);
+
+console.log("=============3==========");
+var isWorking;
+console.log("My Working Status Is:",isWorking);
+isWorking=true;
+console.log("My Working Status Is:",isWorking);
+var isWorkingtypeOf=typeof isWorking;
+console.log("Datatype of Working Status Is: ",isWorkingtypeOf);
+console.log("=============4==========");
+var totalCount="Hundred and seven";
+console.log("=========a============");
+console.log("Totalcount is:",totalCount);
+var totalCounttypeOf=typeof totalCount;
+console.log("Datatype of Totalcount:",totalCounttypeOf);
+console.log("============b===========");
+totalCount=107;
+console.log("Totalcount is:",totalCount); 
+totalCounttypeOf=typeof totalCount;
+console.log("Update value  of Totalcount:",totalCounttypeOf);
+
+
+
+
