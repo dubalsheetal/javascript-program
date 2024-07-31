64bb2e762bd724fcc4f6477790eab3d050a1c674
dubalsheetal <dubalsheetal0190@gmail.com>
2024-07-31 08:24:38 +0530

    Javascript Varibles
    Javascript Varibles

---
+++ 01-varible-assignment.js
@@ -0,0 +1,16 @@
+
+
+
+console.log("My  Name is Shital Chavan");
+
+console.log("My city name is Pune");
+
+console.log("My country name is India");
+
+console.log("My school name is SVKH");
+
+console.log("My collage name is Dr D Y Patil Collage Pune");
+
+
+
+
