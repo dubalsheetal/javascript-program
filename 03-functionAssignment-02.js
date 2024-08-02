05b68d55f063ff56d835e268ae05969a307e8d02
dubalsheetal <dubalsheetal0190@gmail.com>
2024-08-02 18:22:15 +0530

    03-functionAssignment-02(bank details)
    03-functionAssignment-02(bank details)

---
+++ 03-functionAssignment-02.js
@@ -0,0 +1,7 @@
+function bankDetails(bankName,accountNum,location,pinCode){
+console.log("Bank Name:",bankName,"||","Account No:",accountNum,"||","Location :",location,"||","Pincode:",pinCode);
+
+}
+bankDetails("CITY Bank",3456782345,"Pune",431202);
+bankDetails("Axis Bank",7856782345,"Mumbai",441202);
+bankDetails("HDFC BANK",8956782345,"Pune",631202,"Open");
