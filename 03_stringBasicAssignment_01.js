7085f133048cad0c9689987a62515edea01ebe0c
dubalsheetal <dubalsheetal0190@gmail.com>
2024-08-04 19:30:17 +0530

    03_stringBasicAssignment_01.js
    03_stringBasicAssignment_01.js

---
+++ 03_stringBasicAssignment_01.js
@@ -0,0 +1,10 @@
+function stringBasic(){
+    console.log(`My dream company is "Infosys"`);
+}
+stringBasic();
+var myHobby1="Reading";
+var myHobby2="Painting";
+var myHobby3="Traveling";
+console.log(`My hobbies are :` , myHobby1,myHobby2,myHobby3);
+var result=myHobby1.length+myHobby2.length +myHobby3.length;
+console.log('Total Sum Of characters : ' ,result);
