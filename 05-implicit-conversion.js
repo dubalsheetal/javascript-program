d4c097f61eadff502a820662eecad353e567aabe
dubalsheetal <dubalsheetal0190@gmail.com>
2024-08-07 18:05:52 +0530

    05-implicit-cinversion.js
    05-implicit-cinversion.js

---
+++ 05-implicit-conversion.js
@@ -0,0 +1,12 @@
+var result;
+result='3'+2;
+console.log(`Result is : `,result);
+
+result='3'+ true;
+console.log(`Result is : `,result);
+
+result='3'+ undefined;
+console.log(`Result is : `,result);
+
+result='3'+ null;
+console.log(`Result is : `,result);
