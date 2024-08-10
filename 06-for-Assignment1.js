546a4e52b628e1a82ed5f02d88beb762bff12fa1
dubalsheetal <dubalsheetal0190@gmail.com>
2024-08-10 22:27:42 +0530

    06-forLoop-assignment.js
    06-forLoop-assignment.js

---
+++ 06-for-Assignment1.js
@@ -0,0 +1,28 @@
+console.log(`1. Print numbers from  5 to 15 by increasing 1`);
+for (let i = 5; i <= 15; i++) {
+    console.log(i); 
+}
+console.log(`2. Print numbers from  50 to 40 by decreasing 1`);
+for (let i = 50; i >=40; i--) {
+    console.log(i); 
+}
+console.log(`3. To find first 15 odd numbers`);
+for (let i = 1; i <=30; i=i+2) {
+    console.log(i); 
+}
+console.log(`4. To find first 10 even numbers`);
+for (let i = 2; i <= 20; i=i+2) {
+    console.log(i);  
+}
+console.log(`5. Print table of 5`);
+for (let i= 5; i <=50; i=i+5) {
+    console.log(i);
+}
+console.log(`6. Print table of 10`);
+for (let i= 10; i<= 100; i=i+10) {
+    console.log(i);   
+}
+console.log(`7. Print table of 10 in reverse order`);
+for (let i =100; i >= 10; i=i-10) {
+    console.log(i);
+}
